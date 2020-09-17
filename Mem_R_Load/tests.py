a8c2c36badc0ea0f7a81162865c53bd46a47331c
janneyt <janneyt@oregonstate.edu>
2020-09-16 21:04:46 -0700

    Finally finished the recursive stripping functions for input. Have a full suite of tests for it too.

--- Mem_R_Load/tests.py
+++ Mem_R_Load/tests.py
@@ -1,12 +1,14 @@
 import unittest
 from Est_Mem_R_Load import *
 
-LEAD_SPACE = " -h"
 
-class MyFirstTests(unittest.TestCase):
+
+class StripInputTests(unittest.TestCase):
 
     '''
-    Test to make sure it returns a well formed argument correctly.
+    ############################################################################
+    Strip_Input Tests
+    ############################################################################
     '''
     def test_strip_input(self):
         self.assertEqual(strip_input('-h'), '-h')
@@ -19,3 +21,38 @@ class MyFirstTests(unittest.TestCase):
 
     def test_remove_lead_space_strip_input(self):
         self.assertEqual(strip_input(' -h'),'-h')
+
+    def test_remove_tab_strip_input(self):
+        self.assertEqual(strip_input('-h\t'),'-h')
+
+
+    '''
+    ############################################################################
+    Recursive_Remove_Escape Tests
+    ############################################################################
+
+    There's a specific order in which escape sequences are tested:
+    ESCAPE_SEQUENCES = ['\a','\\','\'','\b','\f','\n','\r','\t']
+
+    So, if there's a '\r' in the string, the function should in fact output '-h\ r'
+    if the esc_seq parameter is before the '\r' character.
+
+
+    '''
+    def test_remove_escape(self):
+        self.assertEqual(recursive_remove_escape('-h', None), '-h')
+
+    def test_remove_escape_with_none(self):
+        self.assertEqual(recursive_remove_escape(repr('-h\r'), None), repr('-h\r'))
+
+    def test_remove_car_in_array_escape(self):
+        self.assertEqual(recursive_remove_escape(repr('-h\r'), '\r'), '-h')
+
+    def test_remove_car_early_array_escape(self):
+        self.assertEqual(recursive_remove_escape(repr('-h\r'), '\a'), '-h')
+
+    def test_remove_car_late_array_escape(self):
+        self.assertEqual(recursive_remove_escape(repr('-h\r'), '\t'), '-h')
+
+    def test_escseq_not_in_array(self):
+        self.assertEqual(recursive_remove_escape(repr('-h\r'), '\d'), '-h')
